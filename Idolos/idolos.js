5bbfe7ed6585650c5992ff5ea3a8468f69fdb54e
ExpedienteTalleres <frinitomas15@gmail.com>
2025-06-02 01:41:32 -0300

    V2.2

--- Idolos/idolos.js
+++ Idolos/idolos.js
@@ -25,6 +25,15 @@ const menuHamburguesa = document.querySelector('.menu_hamburguesa');
                 "Daniel Alberto Willington, nacido el 1 de septiembre de 1942, es una leyenda indiscutible del Club Atlético Talleres y del fútbol argentino en general. Apodado 'El Loco' por su impredecible y genial estilo de juego, Willington es ampliamente considerado como uno de los mejores jugadores en la historia del club. Su extraordinaria habilidad técnica, combinada con una visión de juego excepcional, lo convirtieron en un mediocampista ofensivo de elite. Willington no solo era capaz de crear oportunidades de gol con pases precisos, sino que también poseía un notable instinto goleador. Su capacidad para controlar el ritmo del juego y su creatividad en el campo lo hicieron un jugador inolvidable para la afición de Talleres. A lo largo de su carrera, Willington demostró una lealtad inquebrantable al club, rechazando ofertas de equipos más grandes para continuar siendo el ídolo de la T. Su legado trasciende las estadísticas, ya que inspiró a generaciones de jugadores y dejó recuerdos imborrables en todos los que tuvieron el privilegio de verlo jugar.",
             },
             {
+            name: "Luis Adolfo Galván",
+            birthDate: "24-02-1948",
+            image: "idolo/Galvan.jpg",
+            description:
+                "Es una leyenda de Talleres. Jugó 502 partidos con el club, siendo el futbolista con más presencias en su historia. Reconocido por su elegancia y solidez defensiva, fue campeón del mundo con la Selección Argentina en 1978.",
+            fullDescription:
+                "Luis Adolfo Galván nació el 24 de febrero de 1948 en Fernández, Santiago del Estero. Antes de dedicarse al fútbol profesional, se recibió de maestro y ejerció la docencia en su pueblo natal. Comenzó su carrera futbolística en Independiente de Fernández y luego pasó a Unión de Santiago del Estero. En 1970, gracias a la recomendación de un amigo, se incorporó a Talleres de Córdoba, donde debutó el 19 de abril de ese año en un partido contra Argentino Peñarol. Durante sus 12 años en Talleres, Galván se destacó por su elegancia, inteligencia táctica y capacidad para anticiparse a las jugadas. Fue parte fundamental del equipo que obtuvo el subcampeonato en el Torneo Nacional de 1977 y logró múltiples títulos en la Liga Cordobesa de Fútbol. Luis Galván falleció el 5 de mayo de 2025 a los 77 años, dejando un legado imborrable en el fútbol argentino y especialmente en Talleres de Córdoba.",
+            },
+            {
             name: "José Omar Reinaldi",
             birthDate: "27-05-1949",
             image: "idolo/Reinaldi.jpg",
@@ -41,6 +50,24 @@ const menuHamburguesa = document.querySelector('.menu_hamburguesa');
                 "Oviedo es considerado uno de los mejores defensores en la historia de Talleres. Su liderazgo y entrega en el campo lo convirtieron en un referente para el club.",
             fullDescription:
                 "Miguel Ángel Oviedo, nacido el 12 de octubre de 1950, es una figura emblemática en la historia del Club Atlético Talleres. Reconocido como uno de los mejores defensores que ha vestido la camiseta albiazul, Oviedo se destacó por su extraordinaria capacidad defensiva, su visión táctica y su inquebrantable espíritu de lucha. Su liderazgo natural tanto dentro como fuera del campo lo convirtió en un verdadero capitán y referente para sus compañeros y para la afición. Oviedo no solo era un defensor sólido, sino también un jugador que entendía el juego de una manera única, contribuyendo significativamente a la organización del equipo. Su entrega incondicional en cada partido y su identificación con los colores del club lo elevaron al estatus de ídolo. La carrera de Oviedo en Talleres dejó un legado duradero, estableciendo un estándar de excelencia y compromiso para las futuras generaciones de defensores del club.",
+            },
+            {
+            name: "Luis Antonio Ludueña",
+            birthDate: "21-02-1954",
+            image: "idolo/Ludueña.jpg",
+            description:
+                "Luis Antonio Ludueña, apodado 'El Hacha', fue un mediocampista nacido en Córdoba en 1954. Jugó 10 años en Talleres, donde disputó 340 partidos y marcó 113 goles, convirtiéndose en uno de los máximos ídolos del club. Fue reconocido por su potencia, técnica y liderazgo.",
+            fullDescription:
+                "Comenzó su carrera futbolística en el club San Lorenzo de Córdoba y, en 1973, fue adquirido por Talleres por una cifra récord para la época. Debutó en la primera división en 1975 bajo la dirección de Ángel Labruna y se consolidó como titular al año siguiente. Durante sus 10 años en Talleres, Ludueña disputó 340 partidos y anotó 113 goles, convirtiéndose en el cuarto máximo goleador histórico del club. Fue una pieza clave en el equipo que alcanzó la final del Torneo Nacional de 1977 y es recordado por haber marcado el gol del triunfo ante Argentinos Juniors en el debut de Diego Maradona en 1976. Su destacado rendimiento le valió convocatorias a la Selección Argentina dirigida por César Luis Menotti. Fue parte del equipo que disputó el Torneo Preolímpico Sudamericano en 1980, donde Argentina se consagró campeón. Sin embargo, una lesión le impidió participar en el Mundial de 1978."
+            },
+            {
+            name: "José Daniel Valencia",
+            birthDate: "03-08-1955",
+            image: "idolo/Valencia.jpg",
+            description:
+                "José Daniel Valencia, apodado 'La Rana', fue un talentoso mediocampista ofensivo argentino nacido en Jujuy en 1955. Jugó 12 años en Talleres, donde se convirtió en ídolo y figura central del equipo. Fue campeón del mundo con la Selección Argentina en 1978 y es recordado por su elegancia y visión de juego",
+            fullDescription:
+                " José Daniel Valencia comenzó su carrera en Gimnasia y Esgrima de Jujuy. En 1975, se incorporó a Talleres de Córdoba, donde desarrolló la mayor parte de su carrera futbolística. Durante sus 12 años en el club, jugó 344 partidos y marcó 42 goles, convirtiéndose en uno de los máximos ídolos de la institución. Fue una pieza clave en el equipo que obtuvo el subcampeonato en el Torneo Nacional de 1977, tras una destacada campaña. Su habilidad para manejar ambos perfiles y su inteligencia táctica lo destacaron como un mediocampista excepcional. Su desempeño en Talleres le valió la convocatoria a la Selección Argentina, con la que disputó los Mundiales de 1978 y 1982. En el Mundial de 1978, fue titular en los primeros partidos y contribuyó al título obtenido por el equipo dirigido por César Luis Menotti.",
             },
         ]
 
