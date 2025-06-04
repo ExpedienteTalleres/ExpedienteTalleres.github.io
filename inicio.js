632118773954c8de5bef888f42d93cadcadb3438
ExpedienteTalleres <frinitomas15@gmail.com>
2025-06-03 21:21:36 -0300

    V2.3

--- inicio.js
+++ inicio.js
@@ -1,3 +1,4 @@
+
 const additionalQuotes = [
     { text: "El orgullo cordobés" },
     { text: "Amor del pueblo" },
@@ -40,5 +41,23 @@ document.querySelectorAll('.quote-card').forEach(card => {
         setTimeout(() => {
             this.style.transform = '';
         }, 150);
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const btnHamb = document.querySelector('.menu_hamburguesa');
+    const navLista = document.querySelector('.nav__ul');
+    const closeBtn = document.querySelector('.close-menu');
+
+    btnHamb.addEventListener('click', () => {
+        navLista.classList.add('active');
+        btnHamb.classList.add('hide');
+        closeBtn.classList.add('show');
+    });
+
+    closeBtn.addEventListener('click', () => {
+        navLista.classList.remove('active');
+        btnHamb.classList.remove('hide');
+        closeBtn.classList.remove('show');
     });
 });
